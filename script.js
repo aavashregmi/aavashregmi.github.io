66f93bb9e4f78cf4f5b9a48c70939201c1631b18
Aavash Regmi <avashregmi2065@gmail.com>
2025-12-17 19:33:14 +0545

    ok

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const cursor = document.querySelector('.cursor');
 const cursor2 = document.querySelector('.cursor2');
 
 document.addEventListener('mousemove', function(e) {
-    cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+    cursor.style.cssText = cursor2.style.cssText =
+      "left:" + e.clientX + "px; top:" + e.clientY + "px;";
 });
 
 // Add hover effect to interactive elements
@@ -65,3 +66,4 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
